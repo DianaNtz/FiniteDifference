f1521a71e9fdd77d0cc4b30c683a95f6a985e291
DianaNtz <110336168+DianaNtz@users.noreply.github.com>
2025-05-14 11:46:13 +0200

    added example functions

--- finite.py
+++ finite.py
@@ -27,3 +27,18 @@ def first(f):
     for i in range(1,n-1):
         fx[i]=(f[i+1]-f[i-1])/(2*h)
     return fx
+#test derivatives with example functions
+h1f1=np.exp(-x1**2)
+h1f1x=first(h1f1)
+h1af1x=-np.exp(-x1**2)*2*x1
+
+
+h2f1=np.exp(-x2**2)
+h2f1x=first(h2f1)
+h2af1x=-np.exp(-x2**2)*2*x2
+
+
+h3f1=np.exp(-x3**2)
+h3f1x=first(h3f1)
+h3af1x=-np.exp(-x3**2)*2*x3
+
