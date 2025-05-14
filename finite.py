578fd769c8c1356ccf952d864a9549d49164d5b7
DianaNtz <110336168+DianaNtz@users.noreply.github.com>
2025-05-14 11:42:34 +0200

    added code description and  imported libraries

---
+++ finite.py
@@ -0,0 +1,7 @@
+"""
+The code below was written by https://github.com/DianaNtz and
+is an implementation of the Finite Difference Derivatives.
+"""
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
