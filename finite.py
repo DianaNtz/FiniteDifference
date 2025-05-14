9c0b1463214928d055c6039efab98559aeec62ee
DianaNtz <110336168+DianaNtz@users.noreply.github.com>
2025-05-14 11:44:11 +0200

    created grids with 3 different resolutions

--- finite.py
+++ finite.py
@@ -5,3 +5,15 @@ is an implementation of the Finite Difference Derivatives.
 #import libraries
 import numpy as np
 import matplotlib.pyplot as plt
+#create grids with 3 different resolutions.
+nx1=30
+h1=1/(nx1-1)
+x1=np.linspace(0,1,nx1+1)
+
+nx2=nx1*2
+h2=1/(nx2-1)
+x2=np.linspace(0,1,nx2+1)
+
+nx3=nx2*2
+h3=1/(nx3-1)
+x3=np.linspace(0,1,nx3+1)
